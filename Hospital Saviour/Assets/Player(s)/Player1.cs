6947fa72cbd26eda478954c4d5d644901b2bb984
Anoop <b.anoop13@gmail.com>
2023-07-15 15:22:43 -0700

    Player movement
    Installed new Input system. Created Player script + added movement code to script.

---
+++ Hospital Saviour/Assets/Player(s)/Player1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private Player1_Input inputActions;
+    private Rigidbody rbody;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        inputActions = new Player1_Input();
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player_Main.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player_Main.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = inputActions.Player_Main.Movement.ReadValue<Vector2>();
+        rbody.velocity = new Vector3(moveInput.x, 0, moveInput.y) * speed;
+    }
+}
