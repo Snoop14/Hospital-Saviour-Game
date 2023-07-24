ab02535025b258cbe593cf96631e0f6d6a846c02
mecoat <66117150+mecoat@users.noreply.github.com>
2023-07-24 12:37:07 +0100

    Script defining Germ1 patient made and added to patient (in scene)
    Array to hold patient state created.  Script to iterate through the states (on interaction at present, we can add code to define when changes are to be made later  and what to do rather than just debug log the change)
    Problem : Second patient will give up folder, but doesn't change states, first patient changes states, but folder interaction no longer works)

---
+++ Hospital Saviour/Assets/Scripts/PatientTypes/Germ1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Germ1 : BaseInteractable
+{
+    //array to hold states of this patient type
+    private string[] actions = { "waiting", "walking", "bed", "waiting", "soup", "complete" };
+
+    //integer to hold current position in the state array
+    private int actionsVal;
+
+    //string to hold current state
+    private string currState;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set initial values
+        actionsVal = 0;
+        currState = actions[actionsVal];
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        //debug log the surrent state of the patient
+      //  Debug.Log(currState);
+
+    //}
+
+    public override void MainInteract(GameObject playerObject)
+    {
+        //add 1 to the current position
+        actionsVal += 1;
+        //shange the state
+        currState = actions[actionsVal];
+        //Debug log the state
+        Debug.Log(currState);
+    }
+}
