795e58ed7722914570c255e1150c53998e6c2cba
Hassan <hazman.games@gmail.com>
2023-07-25 22:27:23 -0400

    Add State class, Create object dictionary list

--- Hospital Saviour/Assets/GameManager.cs
+++ Hospital Saviour/Assets/GameManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class State
+{
+    public bool interactable = false;
+}
 public class GameManager : MonoBehaviour
 {
-    
+
     [SerializeField]
     GameObject bedPrefab;
     [SerializeField]
@@ -15,26 +20,51 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     Material inActiveMat;
 
+    //this later needs to be determined within the level data
     public int activeBedCount = 2;
-    public int inActiveBedCount = 3;
+    public int inActiveBedCount = 4;
 
-    [Range(0,5)]
-    public float bedSeperation = 4;
+    // bedSeperation 3 looks correct
+    [Range(0, 5)]
+    public float bedSeperation = 3;
 
-    List<GameObject> beds;
+    Dictionary<GameObject, State> objectStates;
+    private void Awake()
+    {
+        objectStates = new Dictionary<GameObject, State>();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        /**
+         * for now call generateObjects in start() function, later we need to change this to be 
+         * called when user clicks on a level, then pass the level data into 
+         * generateObjects function as a parameter
+        **/
+        generateObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateObjects()
+    {
         //Instatiate inactive beds
-        for(int i = 0; i < inActiveBedCount; i++)
+        for (int i = 0; i < inActiveBedCount; i++)
         {
-            GameObject newBed = Instantiate(bedPrefab,inActiveBedParent.transform,false);
-            newBed.transform.position += new Vector3(0,0,-bedSeperation * i);
+            GameObject newBed = Instantiate(bedPrefab, inActiveBedParent.transform, false);
+            newBed.transform.position += new Vector3(0, 0, -bedSeperation * i);
             var bedRenderers = newBed.GetComponentsInChildren<MeshRenderer>();
-            foreach(MeshRenderer renderer in bedRenderers)
+            foreach (MeshRenderer renderer in bedRenderers)
             {
                 renderer.material = inActiveMat;
             }
+            //Place bed into object list
+            objectStates[newBed] = new State();
+
         }
 
         //Instatiate Active beds
@@ -42,12 +72,8 @@ public class GameManager : MonoBehaviour
         {
             GameObject newBed = Instantiate(bedPrefab, inActiveBedParent.transform, false);
             newBed.transform.position += new Vector3(0, 0, -bedSeperation * i);
+            //Place bed into object list
+            objectStates[newBed] = new State();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
