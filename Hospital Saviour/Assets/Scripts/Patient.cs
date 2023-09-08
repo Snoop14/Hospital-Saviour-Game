92ae062d7e2c8e13d726ae61256a9538d5323f51
mecoat <66117150+mecoat@users.noreply.github.com>
2023-09-08 17:02:18 +0100

    Fixed the multi-treatment patient revert to 1 bug

--- Hospital Saviour/Assets/Scripts/Patient.cs
+++ Hospital Saviour/Assets/Scripts/Patient.cs
@@ -380,8 +380,9 @@ public class Patient : MonoBehaviour
         {
             icon = healingIcon;
         }
-        else
+        else if (currHeal == 0)
         {
+            Debug.Log(currHeal);
             currHeal = 0;
             healingIcon.sprite = healingOrderIcons[currHeal];
             healingIcon.SetNativeSize();
