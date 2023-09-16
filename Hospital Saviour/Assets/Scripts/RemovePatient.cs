2b25f41db6304e64e4be2dd7b38101cf64685f82
mecoat <66117150+mecoat@users.noreply.github.com>
2023-09-16 11:48:08 +0100

    remove patient comments added

--- Hospital Saviour/Assets/Scripts/RemovePatient.cs
+++ Hospital Saviour/Assets/Scripts/RemovePatient.cs
@@ -10,8 +10,13 @@ public class RemovePatient : MonoBehaviour
 
     private void Start()
     {
+        //find the game manager and set it into the manager variable
         manager = GameObject.Find("Manager").GetComponent<GameManager>();
+
+        //set the patient count variable to be the same as the patient count in the manager
         patientCount = manager.patientCount;
+
+        //set the alue of remove counter to be 0
         removeCounter = 0;
     }
 
@@ -22,15 +27,23 @@ public class RemovePatient : MonoBehaviour
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
+        //if it has the patient tag...
         if(other.tag == "Patient")
         {
+            //remove the patient from the manager list of objects
             manager.RemovePatientFromList(other.gameObject);
+
+            //destroys the patient
             other.GetComponent<Patient>().DestroySelf();
+
+            //increase value of removecounter variable
             removeCounter++;
         }
 
+        //if the remve counter variable is the same as the count of the patients ...
         if(removeCounter == patientCount)
         {
+            //runf the endgame function in the manager
             manager.EndGame();
         }
     }
