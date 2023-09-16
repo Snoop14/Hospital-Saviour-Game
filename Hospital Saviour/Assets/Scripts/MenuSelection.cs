e856873e335dde089cba1b53ebe43f94ccba46b5
mecoat <66117150+mecoat@users.noreply.github.com>
2023-09-16 12:05:25 +0100

    menuselection comments

--- Hospital Saviour/Assets/Scripts/MenuSelection.cs
+++ Hospital Saviour/Assets/Scripts/MenuSelection.cs
@@ -36,6 +36,7 @@ public class MenuSelection : MonoBehaviour
 
     private void Start()
     {
+        //sets active and inactive buttons as per settings
         ResetButtons();
 
         //reset playersset to last value in playerprefs
@@ -43,11 +44,17 @@ public class MenuSelection : MonoBehaviour
         {
             playersSet = PlayerPrefs.GetInt("PlayerNum");
         }
+
+        //gets last level from playerprefs
         levelSet = PlayerPrefs.GetInt("LevelNum");
+        //if the last level is 0 (has not been played)...
         if(levelSet == 0)
         {
+            //the level is 1
             levelSet = 1;
         }
+
+        //find the players slider and set it to the value of players set
         GameObject.Find("PlayersSlider").GetComponent<Slider>().value = playersSet;
     }
 
@@ -57,8 +64,10 @@ public class MenuSelection : MonoBehaviour
     /// <param name="level"></param>
     public void StartLevel(int level)
     {
+        //levelSet value is updated to value of the button
         levelSet = level;
 
+        //runs display high sore for the level
         DisplayHighScore();
     }
 
@@ -71,9 +80,16 @@ public class MenuSelection : MonoBehaviour
     /// <param name="players"></param>
     public void NumberPlayers(Slider players)
     {
+        //set value of playersSet based upon the slider value
         playersSet = (int)players.value;
+
+        //reset level to 1
         levelSet = 1;
+
+        //run rest buttons function
         ResetButtons();
+
+        //run display high score function
         DisplayHighScore();
     }
 
@@ -82,7 +98,10 @@ public class MenuSelection : MonoBehaviour
     /// </summary>
     public void StartGame()
     {
+        //runs SetLevelNum function with the level and player number as set
         SetLevelNum(levelSet, playersSet);
+
+        //loads the gaame
         SceneManager.LoadScene("GameScene");
     }
 
@@ -95,9 +114,13 @@ public class MenuSelection : MonoBehaviour
     /// <param name="_players"></param>
     public static void SetLevelNum(int _level, int _players)
     {
+        //sets the level to load
         levelNum = _level;
+
+        //sets the number of players
         playerNum = _players;
 
+        //sets the player prefs of level and players
         PlayerPrefs.SetInt("LevelNum", levelNum);
         PlayerPrefs.SetInt("PlayerNum", playerNum);
     }
@@ -108,42 +131,63 @@ public class MenuSelection : MonoBehaviour
     /// </summary>
     private void DisplayHighScore()
     {
+        //adds text of level name
         string levelName = "Level " + levelSet;
 
+        //if 1 player mode is selected
         if (playersSet == 1)
         {
+            //add "Solo" to the display words
             infoDisplay.text = levelName + " Solo";
+            
+            // adds 1player value to level name
             levelName += "_1p";
         }
+        //otherwise if there is 2 players selected...
         else if (playersSet == 2)
         {
+            //add "duo" to the displayed text
             infoDisplay.text = levelName + " Duo";
+
+            // adds 2player value to level name
             levelName += "_2p";
         }
 
+        //sets value from player prefs
         currHighScore = PlayerPrefs.GetInt(levelName);
 
+        //sets text for levelname to load
         levelName = "Level" + levelSet + "Data";
+
+        //loads star values for the level
         int[] stars = Resources.Load<Levels>("Level Data/" + levelName).stars;
 
+        //iterate from 0 to 2 (3 iterations)
         for(int i = 0; i <= 2; i++)
         {
+            //display the star values for the level
             StarsDisplay.GetChild(3).GetChild(i).GetComponent<TMP_Text>().text = (stars[i] * playersSet).ToString();
 
+            //if the current high score is higher than the star value...
             if(currHighScore >= (stars[i])*playersSet)
             {
+                //display the star image
                 StarsDisplay.GetChild(i).gameObject.SetActive(true);
             }
+            //otherwise ...
             else
             {
+                //hide the star image
                 StarsDisplay.GetChild(i).gameObject.SetActive(false);
             }
             
         }
 
+        //display the high score value on screen
         scoreDisplay.text = currHighScore.ToString();
     }
 
+    //turned off as only works within Unity
     /*
     /// <summary>
     /// Deletes all playerPrefs and reloads the scene
@@ -181,6 +225,7 @@ public class MenuSelection : MonoBehaviour
             thisButton.interactable = false;
         }
 
+        //turns back on the buttons that are less than or equal to the highest level played plus one (so can play the next level, but no further)
         int numLevelsComplete = PlayerPrefs.GetInt("Highest_Level_Complete_" + playersSet + "p");
         //ensure button unlock remains within set buttons
         if (numLevelsComplete >= 1 && numLevelsComplete <= 4)
@@ -191,6 +236,7 @@ public class MenuSelection : MonoBehaviour
                 thisButton.interactable = true;
             }
         }
+        //if highest level is 5, activate all buttons, but don't try to activate the 6th button, bacause there isn't one
         else if (numLevelsComplete >= 1 && numLevelsComplete == 5)
         {
             for (int i = 1; i <= 4; i++)
